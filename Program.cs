51f3bf6fff593cd5d9d998729083ae43a7ab7782
James Stolte <jamesstolte0879@gmail.com>
2022-06-20 16:28:07 -0600

    finished program

--- Program.cs
+++ Program.cs
@@ -16,48 +16,103 @@ namespace ticTacToe
             spaceList.Add("7");
             spaceList.Add("8");
             spaceList.Add("9");
+            bool done = false;
             for(int i = 1; i <= 9; i++)
             {
-                int player = i;
-                static void drawGrid (List<string> spaceList)
-                {
-                    Console.WriteLine($" {spaceList[0]} | {spaceList[1]} | {spaceList[2]} ");
-                    Console.WriteLine("---+---+---");
-                    Console.WriteLine($" {spaceList[3]} | {spaceList[4]} | {spaceList[5]} ");
-                    Console.WriteLine("---+---+---");
-                    Console.WriteLine($" {spaceList[6]} | {spaceList[7]} | {spaceList[8]} ");
-                    
-                }
-
-                static string playerInput()
-                {
-                    Console.WriteLine("Enter in a number 1-9:  ");
-                    string input = Console.ReadLine();
-                    return input;
-                }
-                
-                static string currentPlayer(int player)
-                {
-                    
-                    if (player == 1 || player == 3 || player == 5 || player == 7 || player == 9)
+                if(done == false)
+                {   
+                    if(i == 1)
                     {
-                        string replace = "X";
-                        player = player + 1;
-                        return replace;
+                        drawGrid(spaceList);
                     }
-                    else
+                    int player = i;
+                    static void drawGrid (List<string> spaceList)
                     {
-                        string replace = "O";
-                        player = player + 1;
-                        return replace;
+                        Console.WriteLine($" {spaceList[0]} | {spaceList[1]} | {spaceList[2]} ");
+                        Console.WriteLine("---+---+---");
+                        Console.WriteLine($" {spaceList[3]} | {spaceList[4]} | {spaceList[5]} ");
+                        Console.WriteLine("---+---+---");
+                        Console.WriteLine($" {spaceList[6]} | {spaceList[7]} | {spaceList[8]} ");
+
                     }
-                    
+
+                    static string playerInput()
+                    {
+                        Console.WriteLine("Enter in a number 1-9:  ");
+                        string input = Console.ReadLine();
+                        return input;
+                    }
+
+                    static string currentPlayer(int player)
+                    {
+
+                        if (player == 1 || player == 3 || player == 5 || player == 7 || player == 9)
+                        {
+                            string replace = "X";
+                            player = player + 1;
+                            return replace;
+                        }
+                        else
+                        {
+                            string replace = "O";
+                            player = player + 1;
+                            return replace;
+                        }   
+                    }
+
+                    static List<string> updateList(List<string> spaceList, string space, string replace)
+                    {
+                        for(int i = spaceList.Count - 1; i >=0; i--)
+                        {
+                            if (spaceList[i].Contains(space))
+                            {
+                                spaceList[i] = replace;
+                            }
+                        }
+                        return spaceList;
+                    }
+                    static bool checkIfWin(List<string> spaceList)
+                    {
+                        if(spaceList[0] == spaceList[1] && spaceList[0] == spaceList[2])
+                        {
+                            return true;
+                        }
+                        if(spaceList[0] == spaceList[3] && spaceList[0] == spaceList[6])
+                        {
+                            return true;
+                        }if(spaceList[4] == spaceList[1] && spaceList[0] == spaceList[8])
+                        {
+                            return true;
+                        }if(spaceList[1] == spaceList[4] && spaceList[1] == spaceList[7])
+                        {
+                            return true;
+                        }if(spaceList[2] == spaceList[5] && spaceList[2] == spaceList[8])
+                        {
+                            return true;
+                        }if(spaceList[2] == spaceList[4] && spaceList[2] == spaceList[6])
+                        {
+                            return true;
+                        }if(spaceList[3] == spaceList[4] && spaceList[3] == spaceList[5])
+                        {
+                            return true;
+                        }if(spaceList[6] == spaceList[7] && spaceList[6] == spaceList[8])
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+
+                    string space = playerInput();
+                    string replace = currentPlayer(player);
+                    spaceList = updateList(spaceList, space, replace);
+                    drawGrid(spaceList);
+                    bool yeet = checkIfWin(spaceList);
+                    if (yeet == true)
+                    {
+                        Console.WriteLine($"Player {replace} Wins!");
+                        done = true;
+                    }
                 }
-                drawGrid(spaceList);
-                string space = playerInput();
-                string replace = currentPlayer(player);
-
-
             }
             
         }
